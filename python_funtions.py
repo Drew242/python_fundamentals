8e42dfd7c01df3a92181ef9006ef679ef2dc4f18
Drew Conly <atconly@gmail.com>
2016-05-10 19:40:26 -0600

    be more explicit, but list alternative input option

--- python_funtions.py
+++ python_funtions.py
@@ -2,11 +2,15 @@
 #splits the string into a list of names
 #prints to the console "Hello name" for each name
 
-names = input("Some names please ")
+
+# this is an easier way of grabbing input, but less explicit
+# names = input("Some names please ")
+print("Some names please ")
+names = input()
 
 def hello_names(names):
     list_names = names.split()
     for name in list_names:
-        print("Hello" + " " + name)
+        print("Hello," + " " + name)
 
 hello_names(names)
